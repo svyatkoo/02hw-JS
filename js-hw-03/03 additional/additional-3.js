40258ad377f22f81637001a00f385256d775ffac
SviatoslavHeletii <svyatkoo@gmail.com>
2021-11-17 01:12:22 +0200

    hw+cw+add(full)

--- js-hw-03/03 additional/additional-3.js
+++ js-hw-03/03 additional/additional-3.js
@@ -212,39 +212,6 @@
 
 
 // - Дано 2 масиви з рівною кількістю об'єктів.
-// Масиви:
-// let usersWithId = [
-//     {id: 1, name: 'vasya', age: 31, status: false},
-//     {id: 2, name: 'petya', age: 30, status: true},
-//     {id: 3, name: 'kolya', age: 29, status: true},
-//     {id: 4, name: 'olya', age: 28, status: false}
-// ];
-//
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ];
-//
-// let usersWithCities = [];
-// let a = usersWithId;
-// let b = citiesWithId;
-// let ttt = function fun(a,b) {
-//     let newObj = {};
-//     for (let key in a) {
-//         if (a[key] === b[key]) {
-//             newObj = a.merge(b);
-//         }
-//     }
-//     return newObj;
-// }
-//
-// // let ttt = fun(usersWithId, citiesWithId);
-// usersWithCities.push(ttt);
-// console.log(usersWithCities);
-
-//
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
 // Example:
@@ -263,9 +230,39 @@
 //             // TO BE CONTINUED .....
 //         ]
 //
+// Масиви:
+
+// {
+//     let usersWithId = [
+//         {id: 1, name: 'vasya', age: 31, status: false},
+//         {id: 2, name: 'petya', age: 30, status: true},
+//         {id: 3, name: 'kolya', age: 29, status: true},
+//         {id: 4, name: 'olya', age: 28, status: false}
+//     ];
+//
+//     let citiesWithId = [
+//         {user_id: 3, country: 'USA', city: 'Portland'},
+//         {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//         {user_id: 2, country: 'Poland', city: 'Krakow'},
+//         {user_id: 4, country: 'USA', city: 'Miami'}
+//     ];
 //
 //
+//     let usersWithCities = [];
+//     for (let userId in usersWithId) {
+//         for (let citiesId in citiesWithId) {
+//             if (usersWithId[userId]['id'] === citiesWithId[citiesId]['user_id']) {
+//                 usersWithId[userId].address = citiesWithId[citiesId];
+//                 usersWithCities.push(usersWithId[userId]);
+//             }
 //
+//         }
+//     }
+//
+//     console.log(usersWithCities);
+// }
+
+
 // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
 
 // {
