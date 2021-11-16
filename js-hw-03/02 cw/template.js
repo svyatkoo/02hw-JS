f7d792588d25c888937a42338ba7c5c0893deeda
SviatoslavHeletii <svyatkoo@gmail.com>
2021-11-16 23:22:34 +0200

    hw+add

---
+++ js-hw-03/02 cw/template.js
@@ -0,0 +1,118 @@
+<!--        template 1.1-->
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write(`<ul>`)
+for (let i = 0; i < listOfItems.length; i++) {
+    document.write(`<li>${listOfItems[i]}</li>`);
+}
+document.write(`</ul>`)
+
+// <!---------------------------------------------->
+
+// <!-- template 2.1-->
+
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+
+
+document.write(`<div class="simpsons_wrap1">`)
+for (let i = 0; i < simpsons.length; i++) {
+    document.write(`<div class="simpsons_item1"><div><span class="simpsons_item1_decor">
+                        NAME- ${simpsons[i]['name']} <br>
+                        SURNAME- ${simpsons[i]['surname']} <br>
+                        AGE-${simpsons[i]['age']}</span> <br> <br>
+                        INFO-${simpsons[i]['info']}
+                        </div>
+                        <div class="simpsons_phot1"><img alt="photo" src="${simpsons[i]['photo']}"></div>
+                        </div>`);
+}
+document.write(`</div>`)
+
+// <!-- template 2.2--><!--template 2.3-->
+
+document.write(`<div class="simpsons_wrap2">`)
+for (let i = 0; i < simpsons.length; i++) {
+    document.write(`<div class="simpsons_item2"><div>
+                        <h2>NAME- ${simpsons[i]['name']}</h2>
+                        <h2>SURNAME- ${simpsons[i]['surname']}</h2>
+                        <h3>AGE-${simpsons[i]['age']}</h3>
+                        <p>INFO-${simpsons[i]['info']}</p></div>
+                        <div class="simpsons_phot2"><img alt="photo" src="${simpsons[i]['photo']}"></div>
+                        </div>`);
+}
+document.write(`</div>`)
+
+
+// <!---------------------------------------------->
+// <!--        template 3.1-->
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+document.write(`<div class='product_wrap'>`)
+for (i = 0; i < products.length; i++) {
+    document.write(`<div class='product-card'>
+                    <h3 class='product-title'>${products[i]['title']}. Price - ${products[i]['price']}</h3>
+                    <img alt="" class="product-image" src="${products[i]['image']}">
+                </div>`);
+
+}
+
+
+document.write(`</div>`)
--- js-hw-03/03 additional/additional-3.js
+++ js-hw-03/03 additional/additional-3.js
@@ -89,37 +89,36 @@
 // c. Заповнити масив 20ма рандомними числами
 // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732
 
-    // let arr = [];
-    // while (arr.length < 50) {
-    //     let num = Math.floor(Math.random() * 101);
-    //         if (!(num % 2)) {
-    //             arr.push(num);
-    //         }
-    // }
-    // console.log(arr);
-    //
-    // while (arr.length < 100) {
-    //     let num = Math.floor(Math.random() * 101);
-    //     if (num % 2) {
-    //         arr.push(num);
-    //     }
-    // }
-    // console.log(arr);
-    //
-    // while (arr.length < 120) {
-    //     let num = Math.floor(Math.random() * 101);
-    //     arr.push(num);
-    // }
-    // console.log(arr);
-    //
-    // while (arr.length < 140) {
-    //     let num = Math.floor(Math.random() * 1000);
-    //         if (num >= 8 && num <= 732) {
-    //             arr.push(num);
-    //         }
-    // }
-    // console.log(arr);
-
+// let arr = [];
+// while (arr.length < 50) {
+//     let num = Math.floor(Math.random() * 101);
+//         if (!(num % 2)) {
+//             arr.push(num);
+//         }
+// }
+// console.log(arr);
+//
+// while (arr.length < 100) {
+//     let num = Math.floor(Math.random() * 101);
+//     if (num % 2) {
+//         arr.push(num);
+//     }
+// }
+// console.log(arr);
+//
+// while (arr.length < 120) {
+//     let num = Math.floor(Math.random() * 101);
+//     arr.push(num);
+// }
+// console.log(arr);
+//
+// while (arr.length < 140) {
+//     let num = Math.floor(Math.random() * 1000);
+//         if (num >= 8 && num <= 732) {
+//             arr.push(num);
+//         }
+// }
+// console.log(arr);
 
 
 // 2. Вивести за допомогою console.log кожен третій елемен
@@ -199,27 +198,27 @@
 
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
 
-{
-    let abc = ['vas', 2 , 45, 'ads', 9, 'str', 0, 12, -3, 'slovo', 'ttr', 1, {name: 'kolo'}, 21];
-    let arrNumber = [];
-    for (let item of abc) {
-        console.log(item);
-        if (typeof (item) === 'number') {
-            arrNumber.push(item);
-        }
-    }
-    console.log(arrNumber);
-}
+// {
+//     let abc = ['vas', 2 , 45, 'ads', 9, 'str', 0, 12, -3, 'slovo', 'ttr', 1, {name: 'kolo'}, 21];
+//     let arrNumber = [];
+//     for (let item of abc) {
+//         console.log(item);
+//         if (typeof (item) === 'number') {
+//             arrNumber.push(item);
+//         }
+//     }
+//     console.log(arrNumber);
+// }
 
 
 // - Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
-//     let usersWithId = [
-//         {id: 1, name: 'vasya', age: 31, status: false},
-//         {id: 2, name: 'petya', age: 30, status: true},
-//         {id: 3, name: 'kolya', age: 29, status: true},
-//         {id: 4, name: 'olya', age: 28, status: false}
-//     ];
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
 //
 // let citiesWithId = [
 //     {user_id: 3, country: 'USA', city: 'Portland'},
@@ -227,6 +226,24 @@
 //     {user_id: 2, country: 'Poland', city: 'Krakow'},
 //     {user_id: 4, country: 'USA', city: 'Miami'}
 // ];
+//
+// let usersWithCities = [];
+// let a = usersWithId;
+// let b = citiesWithId;
+// let ttt = function fun(a,b) {
+//     let newObj = {};
+//     for (let key in a) {
+//         if (a[key] === b[key]) {
+//             newObj = a.merge(b);
+//         }
+//     }
+//     return newObj;
+// }
+//
+// // let ttt = fun(usersWithId, citiesWithId);
+// usersWithCities.push(ttt);
+// console.log(usersWithCities);
+
 //
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
@@ -249,11 +266,62 @@
 //
 //
 //
-//         - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
-//
+// - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+
+// {
+//     let arr = [1,2,3,4,5,6,7,8,9,10];
+//     for (i = 0; i < arr.length; i++) {
+//         if (!(arr[i]%2)) {
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
 // - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
-//
-//
+
+// {
+//     let arr = [1,2,3,4,5,6,7,8,9,10];
+//     let newArr = [];
+//     i = arr.length-1;
+//     while (i >= 0) {
+//         newArr.push(arr[i]);
+//         i--;
+//     }
+//     console.log(newArr);
+// }
+
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+
+// {
+//     let arr = [ 'a', 'b', 'c'];
+//     word = '';
+//     for (i = 0; i < arr.length; i++) {
+//         word = word + arr[i];
+//     }
+//     console.log(word);
+// }
+
+
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+
+// {
+//     let arr = [ 'a', 'b', 'c'];
+//     word = '';
+//     i = 0;
+//     while (i < arr.length) {
+//         word = word + arr[i];
+//         i++
+//     }
+//     console.log(word);
+// }
+
 // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+
+// {
+//     let arr = [ 'a', 'b', 'c'];
+//     word = '';
+//     for (const item of arr) {
+//         word = word + item;
+//     }
+//     console.log(word);
+// }
